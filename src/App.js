8022ca186e76d404a4c26d1c7627806a846631f2
HameedAsmath <hameedasmath2001@gmail.com>
2023-08-03 14:02:49 +0000

    Project-1 completed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
 import "./App.css";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AppContext } from "./context/AppContext";
 
 
-function App() { 
-  const { wallet, connectWallet, disconnectWallet, getLimitPeopleInEvent, limitPeopleInEvent } = useContext(AppContext);
+function App() {
+  const [limitInput, setLimitInput] = useState()
+  const { wallet, connectWallet, disconnectWallet, getLimitPeopleInEvent, limitPeopleInEvent, setPeopleInEvent, setAttendeeInformation, setName, setAge, setCity, setEmail, setOccupation, getAttendeeList, attendeeList, getTotalPeople, totalPeople } = useContext(AppContext);
   return (
     <div className="App">
-      <button onClick={connectWallet}>Connect</button>
+      <button onClick={connectWallet} className="connect">Connect to Metamask</button>
+      <br></br>
       <p>{wallet}</p>
-      <button className='myButton' onClick={disconnectWallet}>Disconnect Wallet</button>
-      <button onClick={getLimitPeopleInEvent}>Get Limit people</button>
-      <p>{limitPeopleInEvent}</p>
+      <button className="disconnect" onClick={disconnectWallet}>Disconnect Wallet</button>
+      <br />
+      <br></br>
+      <div className="container">
+        <h1>Get Limit people</h1>
+        <button onClick={getLimitPeopleInEvent}>Get Limit people</button>
+        <br></br>
+        <p>{limitPeopleInEvent}</p>
+        <h1>Set Limit People in Event </h1>
+        <input type='number' placeholder='Enter the limit of people who can attend this event' onChange={(e) => (setLimitInput(e.target.value))} className="ml-3" />
+        <button onClick={() => (setPeopleInEvent(limitInput))} >Set Limit of people </button>
+        <br />
+        <br />
+        <h1>Get Attendee List</h1>
+        <button onClick={getAttendeeList} >Get Attendee List</button>
+        <p>{attendeeList}</p>
+        <br />
+        <div className="form">
+          <h1>Add Attendee</h1>
+          <input type='text' placeholder='Enter your name' onChange={(e) => (setName(e.target.value))} />
+          <input type='number' placeholder='Enter your age' onChange={(e) => (setAge(e.target.value))} />
+          <input type='text' placeholder='Enter your occupation' onChange={(e) => (setOccupation(e.target.value))} />
+          <input type='text' placeholder='Enter your city' onChange={(e) => (setCity(e.target.value))} />
+          <input type='text' placeholder='Enter your email' onChange={(e) => (setEmail(e.target.value))} />
+          <button onClick={() => (setAttendeeInformation())} >Set Attendee Information</button>
+        </div>
+        <br />
+        <br />
+        <button onClick={getTotalPeople}>Get Total People</button>
+        <p>{totalPeople}</p>
+      </div>
     </div>
   );
 }
--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,35 +1,44 @@
-import React, {useState, createContext} from 'react';
+import React, { useState, createContext } from 'react';
 import { ContractABI, Contractaddress } from "../utils/AppUtils";
-import {ethers} from "ethers";
+import { ethers } from "ethers";
 
 export const AppContext = createContext();
 
-export default function AppProvider({children}) {
-   const [wallet,setWallet] = useState();
-   const[limitPeopleInEvent,setLimitPeopleInEvent] = useState("")
-   const connectWallet = async() =>{
-    if(!window.ethereum){
+export default function AppProvider({ children }) {
+  const [wallet, setWallet] = useState();
+  const [limitPeopleInEvent, setLimitPeopleInEvent] = useState("")
+  const [attendeeList, setAttendeeList] = useState([])
+  const [totalPeople, setTotalPeople] = useState()
+  // name, age, occupation, city, email
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [occupation, setOccupation] = useState("");
+  const [city, setCity] = useState("");
+  const [email, setEmail] = useState("");
+
+  const connectWallet = async () => {
+    if (!window.ethereum) {
       alert("please download metamask")
       return;
     }
-      const accounts = await window.ethereum.request({method: 'eth_requestAccounts'});
-      setWallet(accounts[0]);
-    }
-    const disconnectWallet = () =>{
-      setWallet("");
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    setWallet(accounts[0]);
   }
-  const getEthereumContract = async () => {
-    if(!window.ethereum){
+  const disconnectWallet = () => {
+    setWallet("");
+  }
+  const getEthereumContract = () => {
+    if (!window.ethereum) {
       alert("Please Download Metamask")
       return
     }
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
-    const contract = new ethers.Contract(Contractaddress,ContractABI,signer)
-    console.log(contract,provider,signer)
+    const contract = new ethers.Contract(Contractaddress, ContractABI, signer)
+    console.log(contract, provider, signer)
     return contract
   };
-   const getLimitPeopleInEvent = async() => {
+  const getLimitPeopleInEvent = async () => {
     try {
       const contract = getEthereumContract()
       const limitPeopleInEvent = await contract.getLimitPeople()
@@ -38,9 +47,52 @@ export default function AppProvider({children}) {
     } catch (error) {
       console.log(error)
     }
-   }
+  }
+  const setPeopleInEvent = async (numberOfPeople) => {
+    try {
+      const contract = getEthereumContract()
+      const tx = await contract.setLimitPeople(numberOfPeople)
+      await tx.wait()
+      console.log(tx)
+      getLimitPeopleInEvent()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  const getAttendeeList = async () => {
+    try {
+      const contract = getEthereumContract();
+      const attendeeList = await contract.getAttendeesList();
+      console.log("exact response", attendeeList);
+      console.log("response converted to String", attendeeList.toString());
+      setAttendeeList(attendeeList.toString());
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  const setAttendeeInformation = async () => {
+    try {
+      const contract = getEthereumContract();
+      const tx = await contract.setAttendDetails(name, age, occupation, city, email);
+      await tx.wait();
+      console.log("transaction mined", tx);
+      getAttendeeList();
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  const getTotalPeople = async () => {
+    try {
+      const contract = getEthereumContract();
+      const totalPeople = await contract.getTotalPeople()
+      console.log("response converted to String", totalPeople.toString());
+      setTotalPeople(totalPeople.toString());
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
-    <AppContext.Provider value={{wallet,setWallet, connectWallet, disconnectWallet, getLimitPeopleInEvent, limitPeopleInEvent}}>
+    <AppContext.Provider value={{ wallet, setWallet, connectWallet, disconnectWallet, getLimitPeopleInEvent, limitPeopleInEvent, setPeopleInEvent, setLimitPeopleInEvent, attendeeList, getAttendeeList, setAttendeeInformation, setName, setAge, setCity, setEmail, setOccupation, getTotalPeople, totalPeople }}>
       {children}
     </AppContext.Provider>
   )
